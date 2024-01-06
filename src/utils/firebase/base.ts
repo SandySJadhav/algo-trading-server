bf49c698e09ca8597d8fc7316b12b8102ad124bd
Sandip Jadhav <jadhavsandipsh@gmail.com>
2024-01-06 20:42:45 +0530

    Develop (#5)
    * init repo
    
    * cleanups
    
    * cleanups
    
    * cleanups
    
    * added cache package
    
    * cleanups
    
    * integrated websockets
    
    * Feature/firestore (#2)
    
    * added firestore for data management
    
    * removed unused firestore package
    
    * updated search action
    
    * fix seach query
    
    * added totp generator libarary for smooth processing
    
    * better comments
    
    ---------
    
    Co-authored-by: Sandip Jadhav <sandip.jadhav@kohler.com>
    
    * Feature/firestore (#3)
    
    * added firestore for data management
    
    * removed unused firestore package
    
    * updated search action
    
    * fix seach query
    
    * added totp generator libarary for smooth processing
    
    * better comments
    
    * capitalize
    
    * fix date issue
    
    * fix search query errors
    
    ---------
    
    Co-authored-by: Sandip Jadhav <sandip.jadhav@kohler.com>
    
    * updated search keywords
    
    * updated logic for keywords
    
    * updates
    
    * fix NFO and MCX
    
    * fix search query
    
    ---------
    
    Co-authored-by: Sandip Jadhav <sandip.jadhav@kohler.com>

--- src/utils/firebase/base.ts
+++ src/utils/firebase/base.ts
@@ -2,160 +2,359 @@ import Cron from "croner";
 import Firebase from "./instance";
 import fetch from "node-fetch";
 import { Timestamp } from "firebase-admin/firestore";
+import { sanitizeText } from "../helpers";
 
-const supportedSegments = ["NSE", "NFO", "MCX"];
+/*
+  const NSE = [
+    "", // stocks - only consider stocks ending with -EQ postfix
+    // "AMXIDX", // not supported
+  ];
+*/
+
+const supportedInstruments: any = {
+  MCX: {
+    OPTFUT: true, // commodity futures
+    FUTCOM: true, // commodity futures
+    // "COMDTY", // not supported
+    // "FUTIDX", // not supported
+    // "AMXIDX", // not supported
+  },
+  NFO: {
+    OPTSTK: true, // stock options
+    OPTIDX: true, // index options
+    FUTSTK: true, // stock futures
+    FUTIDX: true, // index futures
+  },
+};
+
+const months = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
+
+/**
+ *
+ * @param list - String[]
+ * @param text - String
+ * @returns
+ */
+const keywordExists = (list: any = [], text: string) =>
+  !text || list.indexOf(text) !== -1 || list.length >= 30;
+
+type Prop = {
+  token: any;
+  symbol: string;
+  name: string;
+  expiry: string;
+  lotsize: any;
+  instrumenttype: string;
+  exch_seg: string;
+  tick_size: any;
+};
+
+const formatPayload = ({
+  token,
+  symbol,
+  name,
+  expiry,
+  lotsize,
+  instrumenttype,
+  exch_seg,
+  tick_size,
+}: Prop) => {
+  // rel_keywords are related to symbol
+  const rel_keywords: any = [];
+  const expiryDate = new Date(expiry || "12DEC9999");
+  expiryDate.setHours(23, 59, 59, 999);
+
+  if (exch_seg !== "NSE") {
+    // stocks don't have expiry
+    const month = months[expiryDate.getMonth()]; // month = JAN
+    const expDate = expiry.split(month)[0]; // expDate = 31
+    if (!keywordExists(rel_keywords, month)) {
+      rel_keywords.push(month); // JAN
+    }
+    if (!keywordExists(rel_keywords, expDate)) {
+      rel_keywords.push(expDate); // 31
+    }
+    if (["FUTCOM", "FUTSTK", "FUTIDX"].includes(instrumenttype)) {
+      if (!keywordExists(rel_keywords, "FUT")) {
+        rel_keywords.push("FUT");
+      }
+    } else if (["OPTFUT", "OPTSTK", "OPTIDX"].includes(instrumenttype)) {
+      let wrdStr = symbol.substring(name.length);
+      const optionType = wrdStr.endsWith("CE")
+        ? "CE"
+        : wrdStr.endsWith("PE")
+        ? "PE"
+        : "";
+      if (optionType) {
+        if (!keywordExists(rel_keywords, optionType)) {
+          rel_keywords.push(optionType); // CE or PE
+        }
+        // remove CT or PE at end
+        wrdStr = wrdStr.substring(0, wrdStr.length - 2); // output -> MCX = 24FEB7000 and for NFO = 24FEB247000
+        if (instrumenttype === "OPTFUT") {
+          // MCX option
+          wrdStr = wrdStr.substring(5); // output = 7000
+        } else {
+          // NFO option
+          wrdStr = wrdStr.substring(7); // output = 7000
+        }
+        if (!keywordExists(rel_keywords, wrdStr)) {
+          rel_keywords.push(wrdStr); // 7000
+        }
+
+        const midVal: number = parseInt("" + wrdStr.length / 2);
+        const minWordLen: number = midVal > 2 ? midVal : 3;
+
+        for (let i = wrdStr.length; i >= minWordLen; i--) {
+          const word = wrdStr.substring(0, i);
+          if (!keywordExists(rel_keywords, word)) {
+            rel_keywords.push(word);
+          }
+        }
+      } else {
+        console.log(
+          'Something is missing with ["OPTFUT", "OPTSTK", "OPTIDX"], found option with do not ends with CE or PE',
+          symbol
+        );
+      }
+    }
+  }
+
+  return {
+    expiry_timestamp: Timestamp.fromDate(expiryDate),
+    token,
+    symbol,
+    name,
+    lotsize,
+    instrumenttype,
+    exch_seg,
+    expiry,
+    tick_size,
+    rel_keywords,
+  };
+};
+
+const filterInstruments = (instruments: Prop[]) => {
+  return instruments
+    .filter(({ exch_seg, expiry, symbol, instrumenttype }: Prop) => {
+      if (supportedInstruments[exch_seg]?.[instrumenttype] && expiry) {
+        // either NFO or MCX, load only next 1 month of data
+        const expiryDate = new Date(expiry);
+        // set time as day end
+        expiryDate.setHours(23, 59, 59, 999);
+        // get todays date for comparison
+        const todayDate = new Date();
+        // set time as day start
+        todayDate.setHours(0, 0, 0, 0);
+        // get difference in missiseconds
+        const diffTime = expiryDate.valueOf() - todayDate.valueOf();
+        // conver to days
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        // check if difference is not more than 62 days and not less than today's date
+        if (diffDays > 0 && diffDays < 32) {
+          // date difference is not more than 61 days
+          return true;
+        }
+        // expiry date is faar away more than what we need
+        return false;
+      }
+      return (
+        exch_seg === "NSE" &&
+        !instrumenttype &&
+        symbol.endsWith("-EQ") &&
+        !expiry
+      );
+    })
+    .map(
+      ({
+        token,
+        symbol,
+        name,
+        expiry,
+        lotsize,
+        instrumenttype,
+        exch_seg,
+        tick_size,
+      }: Prop) => {
+        return formatPayload({
+          token,
+          symbol,
+          name,
+          expiry,
+          lotsize,
+          instrumenttype,
+          exch_seg,
+          tick_size,
+        });
+      }
+    );
+};
 
 const fetchAllInstruments = async () => {
-    let data: string = "";
-    let response: any = {};
-    console.log("Downloading all instrument data From Angel One...");
-    await new Promise((resolve: any) => {
-        fetch('https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json')
-            .then(response => response.body)
-            .then(res => res.on('readable', () => {
-                let chunk;
-                while (null !== (chunk = res.read())) {
-                    data += chunk.toString();
-                }
-            }).on('end', () => {
-                if (!response.hasError) {
-                    console.log("Downloaded all instrument data From Angel One");
-                    response.instruments = JSON.parse(data);
-                    resolve("SUCCESS");
-                }
-            }))
-            .catch(err => {
-                response.hasError = true;
-                console.log("Downloaded all instrument data From Angel One ended with error", err);
-                resolve("FAILED");
-            });
-    });
-    return response;
+  let data: string = "";
+  let response: any = {};
+  console.log("Downloading all instrument data From Angel One...");
+  await new Promise((resolve: any) => {
+    fetch(
+      "https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json"
+    )
+      .then((response) => response.body)
+      .then((res) =>
+        res
+          .on("readable", () => {
+            let chunk;
+            while (null !== (chunk = res.read())) {
+              data += chunk.toString();
+            }
+          })
+          .on("end", () => {
+            if (!response.hasError) {
+              console.log("Downloaded all instrument data From Angel One");
+              response.instruments = JSON.parse(data);
+              resolve("SUCCESS");
+            }
+          })
+      )
+      .catch((err) => {
+        response.hasError = true;
+        console.log(
+          "Downloaded all instruments from Angel One failed ********",
+          err
+        );
+        resolve("FAILED");
+      });
+  });
+  return response;
 };
 
 const createBatchAndDeleteDocs = async (instruments: any) => {
-    const batch = Firebase.db.batch();
-    instruments.forEach((instrument: any) => {
-        batch.delete(instrument);
-    });
-    return batch.commit();
+  const batch = Firebase.db.batch();
+  instruments.forEach((instrument: any) => {
+    batch.delete(instrument);
+  });
+  return batch.commit();
 };
 
 const createBatchAndPushDocs = async (instruments: any, collection: any) => {
-    // Get a new write batch
-    const batch = Firebase.db.batch();
-    instruments.forEach((instrument: any) => {
-        batch.set(collection.doc(instrument.token), instrument);
-    });
-    return batch.commit();
+  // Get a new write batch
+  const batch = Firebase.db.batch();
+  instruments.forEach((instrument: any) => {
+    batch.set(collection.doc(instrument.token), instrument);
+  });
+  return batch.commit();
 };
 
-const processInstruments = async (instruments: any, collection: any, isDelete: any = false) => {
-    const chunkSize = 500;
-    const allRecords = [];
-    for (let i = 0; i < instruments.length; i += chunkSize) {
-        const chunk = instruments.slice(i, i + chunkSize);
-        if (chunk.length > 0) {
-            if (isDelete) {
-                allRecords.push(createBatchAndDeleteDocs(chunk));
-            } else {
-                allRecords.push(createBatchAndPushDocs(chunk, collection));
-            }
-        }
+const processInstruments = async (
+  instruments: any,
+  collection: any,
+  isDelete: any = false
+) => {
+  const chunkSize = 50;
+  const allRecords = [];
+  for (let i = 0; i < instruments.length; i += chunkSize) {
+    const chunk = instruments.slice(i, i + chunkSize);
+    if (chunk.length > 0) {
+      if (isDelete) {
+        allRecords.push(createBatchAndDeleteDocs(chunk));
+      } else {
+        allRecords.push(createBatchAndPushDocs(chunk, collection));
+      }
     }
-    await Promise.all(allRecords);
-    console.log(isDelete ? "Deleted all records from Firestore" : "Pushed all records to Firestore");
+  }
+  await Promise.all(allRecords);
+  console.log(
+    isDelete
+      ? "Deleted all records from Firestore"
+      : `Pushed all records to Firestore in ${allRecords.length} batches`
+  );
 };
 
 const processDataToFirebase = async () => {
-    const collection = await Firebase.db.collection("instruments");
-    // delete existing data from firestore if already expired
-    const deleteInstrumentList: any[] = [];
+  const collection = await Firebase.db.collection("instruments");
+  // delete existing data from firestore if already expired
+  const deleteInstrumentList: any[] = [];
 
-    try {
-        const dtStart = new Date();
-        dtStart.setHours(0, 0, 0, 0);
-        // find contracts expired at 12AM midnight
-        const docs = await collection.where(
-            "expiry_timestamp",
-            "<",
-            Timestamp.fromDate(dtStart)
-        ).get();
-        docs.forEach((doc: any) => {
-            deleteInstrumentList.push(doc.ref);
-        });
-    } catch (error) {
-        console.log(JSON.parse(JSON.stringify(error)));
+  try {
+    const dtStart = new Date();
+    dtStart.setHours(0, 0, 0, 0);
+    // find contracts expired at 12AM midnight
+    const docs = await collection
+      .where("expiry_timestamp", "<", Timestamp.fromDate(dtStart))
+      .get();
+    // all these documents are going to be delete because they are expired
+    docs.forEach((doc: any) => {
+      deleteInstrumentList.push(doc.ref);
+    });
+  } catch (error) {
+    console.log(JSON.parse(JSON.stringify(error)));
+    return;
+  }
+  if (deleteInstrumentList.length > 0) {
+    // proceed to delete instruments from database;
+    await processInstruments(deleteInstrumentList, collection, true);
+
+    // fetch all instruments from Angel one free json file
+    const { instruments, hasError } = await fetchAllInstruments();
+    if (hasError) {
+      // store this data in Firebase database
+      console.log("Failed to download instruments from Angel One");
+      return;
     }
-    if (deleteInstrumentList.length > 0) {
-        // proceed to delete instruments from database;
-        await processInstruments(deleteInstrumentList, collection, true);
 
-        // fetch all instruments from Angel one free json file
-        const { instruments, hasError } = await fetchAllInstruments();
-        if (hasError) {
-            // store this data in Firebase database
-            console.log("Failed to download instruments from Angel One");
-            return;
-        }
-        // now create new payload to upload new data
-        const selectedInstruments = instruments
-            .filter((instrument: any) => supportedSegments.includes(instrument.exch_seg))
-            .map(({
-                token,
-                symbol,
-                name,
-                expiry,
-                lotsize,
-                instrumenttype,
-                exch_seg,
-                tick_size
-            }: any) => {
-                const dtEnd = new Date(expiry || '12DEC9999');
-                dtEnd.setHours(23, 59, 59, 999);
-                return {
-                    token,
-                    symbol,
-                    name,
-                    lotsize,
-                    instrumenttype,
-                    exch_seg,
-                    expiry,
-                    tick_size,
-                    expiry_timestamp: Timestamp.fromDate(dtEnd)
-                };
-            });
-        if (selectedInstruments?.length > 0) {
-            console.log("Pushing NSE, NFO & MCX records to Firestore ---> Count: ", selectedInstruments.length);
-            await processInstruments(selectedInstruments, collection, false);
-        } else {
-            console.log("No records to delete. Everything already up to date in Firestore");
-        }
+    // now create new payload to upload new data
+    const selectedInstruments = filterInstruments(instruments);
+    if (selectedInstruments?.length > 0) {
+      console.log(
+        "Pushing NSE, NFO & MCX records to Firestore ---> Count: ",
+        selectedInstruments.length
+      );
+      await processInstruments(selectedInstruments, collection, false);
     } else {
-        console.log("No records to delete. Everything already up to date in Firestore");
+      console.log(
+        "No records to delete. Everything already up to date in Firestore"
+      );
     }
+  } else {
+    console.log(
+      "No records to delete. Everything already up to date in Firestore"
+    );
+  }
 };
 
 /**
-        * ┌──────────────── (optional) second (0 - 59)
-        * │ ┌────────────── minute (0 - 59)
-        * │ │ ┌──────────── hour (0 - 23)
-        * │ │ │ ┌────────── day of month (1 - 31)
-        * │ │ │ │ ┌──────── month (1 - 12, JAN-DEC)
-        * │ │ │ │ │ ┌────── day of week (0 - 6, SUN-Mon), (0 to 6 are Sunday to Saturday; 7 is Sunday, the same as 0)
-        * │ │ │ │ │ │
-        * * * * * * *
+ * ┌──────────────── (optional) second (0 - 59)
+ * │ ┌────────────── minute (0 - 59)
+ * │ │ ┌──────────── hour (0 - 23)
+ * │ │ │ ┌────────── day of month (1 - 31)
+ * │ │ │ │ ┌──────── month (1 - 12, JAN-DEC)
+ * │ │ │ │ │ ┌────── day of week (0 - 6, SUN-Mon), (0 to 6 are Sunday to Saturday; 7 is Sunday, the same as 0)
+ * │ │ │ │ │ │
+ * * * * * * *
  */
+
 export const startCronerToSyncInstruments = () => {
-    let maxRuns: any = undefined;
-    let scheduledTimer: string = "0 0 5 * * 1-5";
-    if (process.env.environment === 'dev') {
-        maxRuns = 1;
-        scheduledTimer = "* * * * * *";
-    }
-    // for dev mode, run cron job im
-    Cron(scheduledTimer, { maxRuns }, async () => {
-        // run cron job at 11.30PM in night
-        processDataToFirebase();
-    });
+  let maxRuns: any = undefined;
+  let scheduledTimer: string = "0 0 5 * * 1-5";
+  if (process.env.environment === "dev") {
+    maxRuns = 1;
+    scheduledTimer = "* * * * * *";
+  }
+  // for dev mode, run cron job im
+  Cron(scheduledTimer, { maxRuns }, async () => {
+    // run cron job at 11.30PM in night
+    processDataToFirebase();
+  });
 };
--- src/utils/firebase/search.ts
+++ src/utils/firebase/search.ts
@@ -1,61 +1,83 @@
 import Firebase from "./instance";
 
 type SearchProps = {
-    searchTerm?: string;
+  searchTerm: string;
 };
 
 export const searchInFirestore = async (params: SearchProps) => {
-    try {
-        const { searchTerm } = params;
-        let collection: any = await Firebase.db.collection("instruments");
-        if (searchTerm) {
-            collection = collection.where('symbol', '>=', searchTerm.toUpperCase());
-        }
-        const response = await collection.limit(10).get();
-        if (response.empty) {
-            return {
-                status: "SUCCESS",
-                statusCode: 200,
-                data: []
-            };
-        }
-        const results: any = [];
-        response.forEach((res: any) => {
-            results.push(res.data());
-        });
-        return {
-            status: "SUCCESS",
-            statusCode: 200,
-            data: results
-        };
-    } catch (error) {
-        let responseJSON;
-        try {
-            const jsonRes = JSON.parse(JSON.stringify(error));
-            if (jsonRes.code === 8) {
-                // daily quota exceeded in firestore;
-                responseJSON = {
-                    status: "ERROR",
-                    statusCode: 503,
-                    message: "Service Unavailable",
-                    error: jsonRes
-                };
-            } else {
-                responseJSON = {
-                    status: "ERROR",
-                    statusCode: 500,
-                    message: "Internal Server Error",
-                    error: jsonRes
-                };
-            }
-        } catch (err) {
-            responseJSON = {
-                status: "ERROR",
-                statusCode: 500,
-                message: "Internal Server Error",
-                error
-            };
-        }
-        return responseJSON;
+  try {
+    const { searchTerm } = params;
+    const keywords = searchTerm.toUpperCase();
+    const qry = Firebase.db.collection("instruments");
+    let response;
+    const allKeywords = keywords.split(" ");
+
+    if (allKeywords.length > 1) {
+      response = await qry
+        .where(
+          "rel_keywords",
+          "array-contains-any",
+          keywords.substring(allKeywords[0].length + 1).split(" ")
+        )
+        .orderBy("name")
+        .startAt(allKeywords[0])
+        .endAt(allKeywords[0] + "\uf8ff")
+        .limit(10)
+        .get();
+    } else {
+      response = await qry
+        .orderBy("name")
+        .startAt(allKeywords[0])
+        .endAt(allKeywords[0] + "\uf8ff")
+        .limit(10)
+        .get();
     }
+
+    if (response.empty) {
+      return {
+        status: "SUCCESS",
+        statusCode: 200,
+        data: [],
+      };
+    }
+    const results: any = [];
+    response.forEach((res: any) => {
+      results.push(res.data());
+    });
+    return {
+      status: "SUCCESS",
+      statusCode: 200,
+      data: results,
+    };
+  } catch (error) {
+    console.log(error);
+    let responseJSON;
+    try {
+      const jsonRes = JSON.parse(JSON.stringify(error));
+      if (jsonRes.code === 8) {
+        // daily quota exceeded in firestore;
+        responseJSON = {
+          status: "ERROR",
+          statusCode: 503,
+          message: "Service Unavailable",
+          error: jsonRes,
+        };
+      } else {
+        responseJSON = {
+          status: "ERROR",
+          statusCode: 500,
+          message: "Internal Server Error",
+          error: jsonRes,
+        };
+      }
+    } catch (err) {
+      responseJSON = {
+        status: "ERROR",
+        statusCode: 500,
+        message: "Internal Server Error",
+        error,
+      };
+    }
+    return responseJSON;
+  }
 };
