aeb453b583a4082132968968bc2c0c4aa695ee14
Sandip Jadhav <sandip.jadhav@kohler.com>
2024-01-26 16:22:16 +0530

    cleanup public

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,15 @@ config();
 import { startCronerToSyncInstruments } from './utils/firebase/base';
 import AngelLogin from './utils/angelOne/instance';
 import { onRequest } from 'firebase-functions/v2/https';
-import { logger } from 'firebase-functions/v2';
+import { logger, setGlobalOptions } from 'firebase-functions/v2';
+
+setGlobalOptions({
+  maxInstances: 10,
+  region: 'asia-east2',
+  timeoutSeconds: 10,
+});
 
 const app = express();
-const PORT = process.env.PORT || '3001';
 
 app.use(cors());
 app.use(express.json());
@@ -18,12 +23,15 @@ app.use('/search', searchRouter);
 
 // catch 404
 app.use(function (req, res) {
-  res.redirect('/404.html');
+  res.status(404).send({
+    statusCode: 404,
+    message: '🔥 Resource not found!',
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-  logger.info(`🚀 Server is running on http://localhost:${PORT}`);
+app.listen('3001', () => {
+  console.log(`🚀 Server is running on http://localhost:3001`);
+  logger.info(`🚀 Server is running on http://localhost:3001`);
   // start daily instrument sync job
   startCronerToSyncInstruments();
   // create angel instance and login
