d974e17d7f98a5836985992eeba6369e74c5ec75
Sandip Jadhav <sandip.jadhav@kohler.com>
2024-01-26 07:24:43 +0530

    cleanups

--- src/utils/angelOne/api.ts
+++ src/utils/angelOne/api.ts
@@ -10,9 +10,6 @@ const API = {
     logout: '/rest/secure/angelbroking/user/v1/logout',
 
     order_place: '/rest/secure/angelbroking/order/v1/placeOrder',
-    order_get_book: '/rest/secure/angelbroking/order/v1/getOrderBook',
-    order_modify: '/rest/secure/angelbroking/order/v1/modifyOrder',
-    order_cancel: '/rest/secure/angelbroking/order/v1/cancelOrder',
 
     get_tradebook: '/rest/secure/angelbroking/order/v1/getTradeBook',
     get_rms: '/rest/secure/angelbroking/user/v1/getRMS',
@@ -20,32 +17,8 @@ const API = {
     get_position: '/rest/secure/angelbroking/order/v1/getPosition',
     convert_position: '/rest/secure/angelbroking/order/v1/convertPosition',
 
-    create_rule: '/rest/secure/angelbroking/gtt/v1/createRule',
-    modify_rule: '/rest/secure/angelbroking/gtt/v1/modifyRule',
-    cancel_rule: '/rest/secure/angelbroking/gtt/v1/cancelRule',
-    rule_details: '/rest/secure/angelbroking/gtt/v1/ruleDetails',
-    rule_list: '/rest/secure/angelbroking/gtt/v1/ruleList',
-
     candle_data: '/rest/secure/angelbroking/historical/v1/getCandleData',
-    market_data: '/rest/secure/angelbroking/market/v1/quote',
     search_scrip: '/rest/secure/angelbroking/order/v1/searchScrip',
-    get_all_holding: '/rest/secure/angelbroking/portfolio/v1/getAllHolding',
-    ind_order_details: '/rest/secure/angelbroking/order/v1/details',
-    margin_api: 'rest/secure/angelbroking/margin/v1/batch'
-    // "api.token": "/session/token",
-    // "api.token.invalidate": "/session/token",
-    // "api.token.renew": "/session/refresh_token",
-
-    // "user.margins": "/user/margins",
-    // "user.margins.segment": "/user/margins/{segment}",
-
-    // "orders": "/orders",
-    // "trades": "/trades",
-    // "order.info": "/orders/{order_id}",
-    // "order.place": "/orders/{variety}",
-    // "order.modify": "/orders/{variety}/{order_id}",
-    // "order.cancel": "/orders/{variety}/{order_id}",
-    // "order.trades": "/orders/{order_id}/trades",
 };
 
 export default API;
