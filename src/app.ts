c947a429821bb8ba101564778612b8cf201a13e1
Sandip Jadhav <sandip.jadhav@kohler.com>
2024-02-05 19:17:27 +0530

    remove unnecessary search

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import { config } from 'dotenv';
-import searchRouter from './routes/search';
-import heartBeatRouter from './routes/index';
 config();
 import { startCronerToSyncInstruments } from './utils/firebase/base';
 import AngelLogin from './utils/angelOne/instance';
-import verifyToken from './utils/verifyToken';
 
 // region: 'asia-south1,
 
@@ -14,9 +11,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.use('/', verifyToken, heartBeatRouter);
-app.use('/search', verifyToken, searchRouter);
 
 // catch 404
 app.use(function (req, res) {
